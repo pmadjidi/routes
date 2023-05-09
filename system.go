ab8ae92200dde258b61754e7700a6eba3fc0a64f
pmadjidi <pmadjidi@gmail.com>
2023-05-09 09:57:04 +0200

    check for env variables missing...

--- system.go
+++ system.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"os"
 	"time"
 )
@@ -13,15 +14,32 @@ type system struct {
 }
 
 func createSys() system {
-	TIMEOUT := os.Getenv("TIMEOUT")
-	timeout, err := time.ParseDuration(TIMEOUT)
+	timeout := os.Getenv("TIMEOUT")
+	TIMEOUT, err := time.ParseDuration(timeout)
 	if err != nil {
-		timeout = 10 * time.Second
+		TIMEOUT = 10 * time.Second
 	}
+
+	API_URL := os.Getenv("API_URL")
+	PORT := os.Getenv("PORT")
+	SERVICE_URL := os.Getenv("SERVICE_URL")
+
+	if API_URL == EMPTYSTRING {
+		log.Fatal("env API_URL not set... ")
+	}
+
+	if PORT == EMPTYSTRING {
+		log.Fatal("env PORT not set... ")
+	}
+
+	if SERVICE_URL == EMPTYSTRING {
+		log.Fatal("env SERVICE_URL not set... ")
+	}
+
 	return system{
-		os.Getenv("API_URL"),
-		os.Getenv("PORT"),
-		os.Getenv("SERVICE_URL"),
-		timeout,
+		API_URL,
+		PORT,
+		SERVICE_URL,
+		TIMEOUT,
 	}
 }
