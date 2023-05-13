47c6bce2dc7088ac5e253a44ab25e2e74d22a282
pmadjidi <pmadjidi@gmail.com>
2023-05-13 14:22:40 +0200

    removing old http server

--- httpServer.go
+++ httpServer.go
@@ -7,16 +7,6 @@ import (
 	"os"
 )
 
-func (s *system) startHttpOld() {
-	err := http.ListenAndServe(":"+s.appPort, nil)
-	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Printf("http server is closed...\n")
-	} else if err != nil {
-		fmt.Printf("failed to start the service... : %s\n", err)
-		os.Exit(1)
-	}
-}
-
 func (s *system) startHttp() {
 	err := s.hServer.ListenAndServe()
 	if errors.Is(err, http.ErrServerClosed) {
